aa9959797ee84d7fa454b3ce8c94cdc15ab5aa39
Mikael Svensson <mikael.svensson@klarna.com>
2018-08-19 13:19:58 +0200

    Enable daily backups of database

---
+++ ansible/playbooks/roles/postgres/templates/backup_database.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Backup a Postgresql database into a daily file.
+#
+# Credits: https://gist.github.com/sirbrillig/4624937
+#
+
+BACKUP_DIR=/home/{{ digitalocean_sudo_user }}/database-backups
+DAYS_TO_KEEP=14
+DATABASE={{ app_db_name }}
+SUFFIX=.sql
+USER=postgres
+TIMESTAMP=$(date +"%Y%m%d%H%M")
+FILE="${DATABASE}_backup_${TIMESTAMP}${SUFFIX}"
+
+OUTPUT_FILE=${BACKUP_DIR}/${FILE}
+
+# do the database backup (dump)
+# use this command for a database server on localhost. add other options if need be.
+sudo -u postgres pg_dump -U ${USER} ${DATABASE} -F p -f /tmp/${FILE}
+chmod 0644 /tmp/${FILE}
+mv /tmp/${FILE} ${OUTPUT_FILE}
+
+# gzip the database dump file
+gzip -f $OUTPUT_FILE
+
+chown {{ digitalocean_sudo_user }} ${OUTPUT_FILE}.gz
+
+# prune old backups
+find $BACKUP_DIR -maxdepth 1 -mtime +$DAYS_TO_KEEP -name "*${SUFFIX}.gz" -exec rm -rf '{}' ';'
