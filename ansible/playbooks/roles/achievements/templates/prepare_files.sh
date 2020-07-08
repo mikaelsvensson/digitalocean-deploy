4acf47b10c6d560cd9f7aa9c257bbb5691d446b4
Mikael Svensson <mikael.svensson@klarna.com>
2020-07-08 14:20:59 +0200

    Deploy new app bundle and Nginx config

--- ansible/playbooks/roles/achievements/templates/prepare_files.sh
+++ ansible/playbooks/roles/achievements/templates/prepare_files.sh
@@ -2,5 +2,7 @@
 mkdir -p /var/www/{{app_host_name}}/html
 cp -R ./achievements-bundle/gui/* /var/www/{{app_host_name}}/html/
 chown -R {{ digitalocean_sudo_user }}:{{ digitalocean_sudo_user }} /var/www/{{app_host_name}}/html
+chown -R {{ digitalocean_sudo_user }}:{{ digitalocean_sudo_user }} ./achievements-bundle
 chmod -R 0755 /var/www
-ln -s /etc/nginx/sites-available/{{app_host_name}} /etc/nginx/sites-enabled/
+chmod 0755 ./achievements-bundle/server/start.sh
+ln -s /etc/nginx/sites-available/{{app_host_name}} /etc/nginx/sites-enabled/{{app_host_name}}
