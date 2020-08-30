1be018ea9ab451dead567b4f4c02c19c9aab74a0
Mikael Svensson <mikael.svensson@klarna.com>
2020-08-30 22:48:57 +0200

    Deploy Names app - refactor Achievements app

--- ansible/playbooks/roles/achievements/templates/prepare_files.sh
+++ ansible/playbooks/roles/achievements/templates/prepare_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-mkdir -p /var/www/{{app_host_name}}/html
-cp -R ./achievements-bundle/gui/* /var/www/{{app_host_name}}/html/
-chown -R {{ digitalocean_sudo_user }}:{{ digitalocean_sudo_user }} /var/www/{{app_host_name}}/html
+mkdir -p /var/www/{{ app_host_name }}/html
+cp -R ./achievements-bundle/gui/* /var/www/{{ app_host_name }}/html/
+chown -R {{ digitalocean_sudo_user }}:{{ digitalocean_sudo_user }} /var/www/{{ app_host_name }}/html
 chown -R {{ digitalocean_sudo_user }}:{{ digitalocean_sudo_user }} ./achievements-bundle
-chmod -R 0755 /var/www
+chmod -R 0755 /var/www/{{  app_host_name  }}/html
 chmod 0755 ./achievements-bundle/server/start.sh
-ln -s /etc/nginx/sites-available/{{app_host_name}} /etc/nginx/sites-enabled/{{app_host_name}}
+ln -s /etc/nginx/sites-available/{{ app_host_name }} /etc/nginx/sites-enabled/{{ app_host_name }}
