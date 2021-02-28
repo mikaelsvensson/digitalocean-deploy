23244d960695bfa9f6e2fd68b34fe1fceebc0230
Mikael Svensson <mikael.svensson@klarna.com>
2021-02-28 11:56:32 +0100

    Names: Add start-up parameters for new name importers

--- ansible/playbooks/roles/names/templates/start_names.sh
+++ ansible/playbooks/roles/names/templates/start_names.sh
@@ -6,6 +6,9 @@ IS_BOOTING=$1
   --spring.liquibase.enabled=$IS_BOOTING \
   --scbImporter.onStart=$IS_BOOTING \
   --ssaImporter.onStart=$IS_BOOTING \
+  --ssbImporter.onStart=$IS_BOOTING \
+  --dstImporter.onStart=$IS_BOOTING \
+  --avoindataImporter.onStart=$IS_BOOTING \
   --syllableUpdater.onStart=$IS_BOOTING >> service.log 2>&1
 
 # Documentation for ZGC (The Z Garbage Collector): https://wiki.openjdk.java.net/display/zgc/Main
